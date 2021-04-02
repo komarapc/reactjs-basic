f8cc1041cd5a6f5440d83dbdf3d3b119ae62d186
Komar <komar.izmi@gmail.com>
2021-04-02 14:47:49 +0700

    dynamic values

--- src/home.tsx
+++ src/home.tsx
@@ -1,15 +1,34 @@
-import { Fragment } from "react"
+import { Fragment } from "react";
 
-const Home = ()=>{
-  return(
+const Home = () => {
+  const title: string = "Home Components";
+  const persons: { id: number; person_name: string; email: string }[] = [
+    {
+      id: 0,
+      person_name: "Komar",
+      email: "komar.izmi@gmail.com",
+    },
+    {
+      id: 1,
+      person_name: "Haruka",
+      email: "haruka@gmail.com",
+    },
+  ];
+
+  return (
     <Fragment>
       <div className="container">
         <h1 className="display-4">
-          Home Component
+          {title} <br />
         </h1>
+        <p>
+          {persons.map((person, i) => {
+            return <div>{person.email}</div>;
+          })}
+        </p>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
