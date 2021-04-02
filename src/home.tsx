17b1c594cdd611043dadd8f9b496e076ecbfce4a
Komar <komar.izmi@gmail.com>
2021-04-02 14:10:19 +0700

    create component home

---
+++ src/home.tsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+const Home = ()=>{
+  return(
+    <Fragment>
+      <div className="container">
+        <h1 className="display-4">
+          Home Component
+        </h1>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Home
